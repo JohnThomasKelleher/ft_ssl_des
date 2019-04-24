471405afd0a1c65e1022b85f4ba6d8bfdecace0b
John Kelleher <jkellehe@e1z2r4p28.42.us.org>
2019-04-24 16:09:53 -0700

    i am commiting here because i am too lazy to make a remote

--- acc_des.c
+++ acc_des.c
@@ -12,10 +12,11 @@ void    initial_perm(t_flags *f)
   int i = 0; //is this right
   uint64_t x;
   uint64_t rev = 0;
-
+  uint64_t hold = 0;
   f->ww = (uint64_t*)f->file;
   
   f->x = f->ww[0];
+  //printf("before ip: %llu, ", f->x);
   while (i < 64)
     {
       x = f->x;
@@ -27,9 +28,11 @@ void    initial_perm(t_flags *f)
       i++;
     }
   f->x = rev; 
+  //printf("after ip: %llu\n", f->x);
   f->ar = ((f->x << 32) >> 32);
-  f->l = ((f->x >> 32) << 32);
-  //printf("initial perm f->x%llu f->ar%u f->l%u\n", (unsigned long long)f->x, f->ar, f->l);
+  hold = ((f->x >> 32) << 32);
+  f->l = (hold >>32);
+  //printf("left: %u, right: %u\n", f->l, f->ar);
 }
 
 void final_perm(t_flags *f)
@@ -90,11 +93,14 @@ uint32_t	rr_28(uint32_t n, uint32_t d)
 void	parity_drop(t_flags *f)
 {
   int a[56] = {
-    57, 49, 41, 33, 25, 17, 9, 1, 58, 50, 42, 34, 26, 
-    18, 10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 
-    44, 36, 63, 55, 47, 39, 31, 23, 15, 7, 62, 54, 46, 
-    38, 30, 22, 14, 6, 61, 53, 45, 37, 29, 21, 13, 5, 
-    28, 20, 12, 4
+    57, 49, 41, 33, 25, 17, 9,
+    1, 58, 50, 42, 34, 26, 18,
+    10, 2, 59, 51, 43, 35, 27,
+    19, 11, 3, 60, 52, 44, 36,
+    63, 55, 47, 39, 31, 23, 15,
+    7, 62, 54, 46, 38, 30, 22,
+    14, 6, 61, 53, 45, 37, 29,
+    21, 13, 5, 28, 20, 12, 4
   };
   int i = 0;//is this right
   uint64_t x;
@@ -104,10 +110,9 @@ void	parity_drop(t_flags *f)
   while (i < 56)
     {
       x = f->in_key;
-      x<<= (a[i] - 1);
-      //x &= 0x8000000000000000;//replace with macro
-      x>>= 55;
-      x<<= (56- (i) -1 );//
+      x <<= (a[i] - 1);
+      x >>= 63;
+      x <<= (55- (i));//
       new |= x;
 
       i++;
@@ -119,11 +124,14 @@ void	parity_drop(t_flags *f)
 uint64_t	comp_dbox(uint64_t b)
 {
   int a[48] = {
-    14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 
-    10, 23, 19, 12, 4, 26, 8, 16, 7, 
-    27, 20, 13, 2, 41, 52, 31, 37, 47, 
-    55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 
-    56, 34, 53, 46, 42, 50, 36, 29, 32
+    14, 17, 11, 24, 1, 5,
+    3, 28, 15, 6, 21, 10,
+    23, 19, 12, 4, 26, 8,
+    16, 7, 27, 20, 13, 2,
+    41, 52, 31, 37, 47, 55,
+    30, 40, 51, 45, 33, 48,
+    44, 49, 39, 56, 34, 53,
+    46, 42, 50, 36, 29, 32
   };
   int i = 0;
   uint64_t hold;
@@ -132,10 +140,10 @@ uint64_t	comp_dbox(uint64_t b)
   while (i < 48)
     {
       hold = b;
-      hold <<= (a[i] - 1);
       //hold &= 0x8000000000000000;//replace with macro
-      hold >>= 47;
-      hold <<= (48- (i) - 1);//
+      hold >>= (56 - a[i]);
+      hold &= 1;
+      hold <<= (47 - (i));//
       new |= hold;
 
       i++;
@@ -202,23 +210,33 @@ void	generate_keys_des(t_flags *f)
 
   //f->in_key = 0x4080808080808080;
   //read_key(f);
+  //printf("before parity: %llx\n", f->in_key);
   parity_drop(f);
-  left = (f->in_key >> 36);// << 36);
-  right = ((f->in_key << 28) >> 28);
+  //printf("after parity: %llx\n", f->in_key);
+  left = (f->in_key >> 28);// << 36);
+  right = (f->in_key & 0xfffffff);
+  //printf("left: %x, right %x\n", left, right);
   
   while (i < 16)
     {
-      shift = (i == 0 || i == 1 || i == 8 || i == 16) ? (1) : (2);
+      shift = (i == 0 || i == 1 || i == 8 || i == 15) ? (1) : (2);
+     
+      //printf("before: left: %x, right %x\n", left, right);
       left = lr_28(left, shift);
       right = lr_28(right, shift);
-
-      hold |= left;
+      //printf("shifted: %d, left: %x, right %x\n\n",shift,  left, right);
+      hold = 0;
+      hold |= (left);
+      hold <<= 28;
       hold |= right;
+      //printf("before box: %llx, ", hold);
+
       hold = comp_dbox(hold);
+      //printf("after box: %llx\n", hold);
       f->keys[i] = hold;
      i++;
     }
-  
+  //print_keys(f);
 }
 
 
--- main_des.c
+++ main_des.c
@@ -3,26 +3,36 @@
 
 void	expansion_des(t_flags *f)
 {
-  int a[64] = {
-    32, 1, 2, 3, 4, 5, 4, 5, 6, 7, 8, 9, 8, 9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25, 24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32, 1
+  int a[48] = {
+    32, 1, 2, 3, 4, 5,
+    4, 5, 6, 7, 8, 9,
+    8, 9, 10, 11, 12, 13,
+    12, 13, 14, 15, 16, 17, 
+    16, 17, 18, 19, 20, 21,
+    20, 21, 22, 23, 24, 25,
+    24, 25, 26, 27, 28, 29,
+    28, 29, 30, 31, 32, 1
   };
-  int i = 1; //is this right
+  int i = 0; //is this right
   uint64_t x;
   uint64_t res = 0;
   uint32_t r_hold;
-
-  while (i < 64)
+  
+  f->ex_r = 0;
+  //printf("before exp: %u, ", f->ar);
+  while (i < 48)
     {
       r_hold = f->ar;
-      r_hold <<= a[i];
+      r_hold <<= (a[i] - 1);
       r_hold >>= 31;
       x = r_hold;
-      x <<= (48 - (i) - 1);//
+      x <<= (47 - (i));//
       res |= x;
 
       i++;
     }
   f->ex_r = res;
+  //printf("after exp: %llu\n", f->ex_r);
 }
 
 void	s_boxes(t_flags *f)
@@ -76,15 +86,28 @@ void swap_lr(t_flags *f)
   f->ar = hold;
 }
 
+void	print_keys(t_flags *f)
+{
+  int i = 0;
+  while (i < 16)
+    {
+      printf("i: %d, key: %llx\n", i, f->keys[i]);
+      i++;
+    }
+}
+
 void	ft_16_rounds(t_flags *f)
 {
   int i = 0;
   while(i < 16)
     {
+    
       expansion_des(f); //does expansion from f->r to f->ex_r
       //f->ar = 0;
       f->ex_r ^= f->keys[i];
+      printf("i: %d, before sbox: %llx, ", i, f->ex_r);
       s_boxes(f); //does sboxes to go from f->ex_r to f->f_hold
+      printf("after sbox: %x\n", f->f_hold);
       str8_d_box(f); //does straight dboxing on f_hold
       f->l ^= f->f_hold;
       if (i != 15)
@@ -136,38 +159,63 @@ void	handle_des(t_flags *f, char **a)
   ft_des(f);
 }
 
+void	flip_buf(char *buf)
+{
+  char hold;
+  hold = buf[0];
+  buf[0] = buf[7];
+  buf[7] = hold;
+  hold = buf[1];
+  buf[1] = buf[6];
+  buf[6] = hold;
+  hold = buf[2];
+  buf[2] = buf[5];
+  buf[5] = hold;
+  hold = buf[3];
+  buf[3] = buf[4];
+  buf[4] = hold;
+}
+
 void   handle_shit(char *buf, t_flags *f)
 {
   uint64_t *hold;
   
   hold = (uint64_t*) buf;
+  flip_buf(buf);
+  //printf("before, data: %llu, as string: %s, after:", hold[0], buf);
   hold[0] ^= f->x;
+  //printf("data: %llu, iv: %llu\n", hold[0], f->x);
 }
 
 void	ft_des(t_flags *f)
 {
   //printf("entering des, fd = %d\n", f->fd);
   char buf[9];
-  
+  int i = 0;
+
   f->keys = (uint64_t*)malloc(sizeof(int) * 16);
   generate_keys_des(f);
   while (8 == (f->ret = read(f->fd, buf, 8)))
     {
+      //printf("i: %d", i);
       handle_shit(buf, f);
-      //printf("%s\n", buf);
       buf[f->ret] = '\0';
       f->file = buf;
   initial_perm(f);
   ft_16_rounds(f);
   final_perm(f);
   (f->ecb) ? (print_cipher(f)) : (0);
+  i++;
     }
   (!f->ecb) ? (print_cipher(f)) : (0);
   f->file = buf;
   pkcs7_pad(f);
+  //printf("i: %d, ", i);
+  
   handle_shit(buf, f);
-  //printf("after padding\n");
-  initial_perm(f);
+  
+initial_perm(f);
+ 
   ft_16_rounds(f);
   final_perm(f);
   print_cipher(f);
